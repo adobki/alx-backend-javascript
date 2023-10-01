52a97094d01776799a8cd5eea616a5cb27fc9a95
Donald Ajaps <95923018+adobki@users.noreply.github.com>
2023-10-01 22:41:17 +0100

    Optimise task 7 in 0x01: ES6 Promises
    Optimise functionality of loadBalancer() function in 7-load-balancer.js as follows:
    - Remove all previous code
    + Use inbuilt race method of Promise object that returns result of first completed promise and discards others.

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,13 +1,5 @@
 // Returns output from promise that gets resolved first
 
 export default async function loadBalancer(chinaDownload, USDownload) {
-  const completedJobs = [];
-
-  USDownload.then((data) => completedJobs.push(data));
-  chinaDownload.then((data) => completedJobs.push(data));
-
-  await USDownload.then();
-  await chinaDownload.then();
-
-  return completedJobs[0];
+  return Promise.race([chinaDownload, USDownload]);
 }
